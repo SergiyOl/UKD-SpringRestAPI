f1d3afb4b738e8fa5a7160593098a26c3f1240f9
SergiyOl <ser4002@gmail.com>
2025-03-31 20:51:10 +0300

    Added update and delete controllers

--- src/main/java/com/springrest/rest/controller/HomeRestController.java
+++ src/main/java/com/springrest/rest/controller/HomeRestController.java
@@ -3,8 +3,10 @@ package com.springrest.rest.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,9 +34,28 @@ public class HomeRestController {
         return studentService.getStudentById(id);
     }
 
-    @PostMapping("/students/")
+    @PostMapping("/students")
     public ResponceStudentDTO addStudent(@RequestBody RequestStudentDTO requestDTO) {
-        studentService.addStudent(requestDTO.getId(), requestDTO.getName(), requestDTO.getAge());
-        return (new ResponceStudentDTO(requestDTO.getId(), requestDTO.getName(), requestDTO.getAge()));
+        Optional<Student> responce = studentService.addStudent(requestDTO.getId(), requestDTO.getName(),
+                requestDTO.getAge());
+        if (responce != null)
+            return (new ResponceStudentDTO(requestDTO.getId(), requestDTO.getName(), requestDTO.getAge()));
+        else
+            return null;
+    }
+
+    @PutMapping("/students")
+    public ResponceStudentDTO updateStudent(@RequestBody RequestStudentDTO requestDTO) {
+        Optional<Student> responce = studentService.updateStudent(requestDTO.getId(), requestDTO.getName(),
+                requestDTO.getAge());
+        if (responce != null)
+            return (new ResponceStudentDTO(requestDTO.getId(), requestDTO.getName(), requestDTO.getAge()));
+        else
+            return null;
+    }
+
+    @DeleteMapping("/students/")
+    public Optional<Student> deleteStudentById(@RequestParam(required = true) Long id) {
+        return studentService.deleteStudentById(id);
     }
 }
