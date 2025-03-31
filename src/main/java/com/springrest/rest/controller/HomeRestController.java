c38888649dc79174cbb1f2f915b4c9d99f7cbbf5
SergiyOl <ser4002@gmail.com>
2025-03-31 20:38:57 +0300

    Added DTO post controller

--- src/main/java/com/springrest/rest/controller/HomeRestController.java
+++ src/main/java/com/springrest/rest/controller/HomeRestController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springrest.rest.model.RequestStudentDTO;
+import com.springrest.rest.model.ResponceStudentDTO;
 import com.springrest.rest.model.Student;
 import com.springrest.rest.service.StudentService;
 
@@ -24,7 +28,13 @@ public class HomeRestController {
     }
 
     @GetMapping("/students/")
-    public Optional<Student> getStudentById(@RequestParam(required = false) Long id) {
+    public Optional<Student> getStudentById(@RequestParam(required = true) Long id) {
         return studentService.getStudentById(id);
+    }
+
+    @PostMapping("/students/")
+    public ResponceStudentDTO addStudent(@RequestBody RequestStudentDTO requestDTO) {
+        studentService.addStudent(requestDTO.getId(), requestDTO.getName(), requestDTO.getAge());
+        return (new ResponceStudentDTO(requestDTO.getId(), requestDTO.getName(), requestDTO.getAge()));
     }
 }
---
+++ src/main/java/com/springrest/rest/model/ResponceStudentDTO.java
@@ -0,0 +1,14 @@
+package com.springrest.rest.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponceStudentDTO {
+    private Long id;
+    private String name;
+    private int age;
+}
