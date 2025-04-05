856d3d303991df4140228e8fbec2a47bc648f806
SergiyOl <ser4002@gmail.com>
2025-04-05 17:52:01 +0300

    Added loging to HomeRestController

--- src/main/java/com/springrest/rest/controller/HomeRestController.java
+++ src/main/java/com/springrest/rest/controller/HomeRestController.java
@@ -3,6 +3,8 @@ package com.springrest.rest.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +23,7 @@ import jakarta.servlet.http.HttpServletResponse;
 @RestController
 public class HomeRestController {
     private StudentServiceImpl studentService = null;
+    private static final Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     public HomeRestController(StudentServiceImpl studentService) {
         this.studentService = studentService;
@@ -28,16 +31,19 @@ public class HomeRestController {
 
     @GetMapping("/students")
     public List<Student> listStudents() {
+        logger.info("Received GET request to /students");
         return studentService.getAllStudents();
     }
 
     @GetMapping("/students/")
     public Optional<Student> getStudentById(@RequestParam(required = true) Long id) {
+        logger.info("Received GET request to /students/?id=" + id);
         return studentService.getStudentById(id);
     }
 
     @PostMapping("/students")
     public ResponceStudentDTO addStudent(@RequestBody RequestStudentDTO requestDTO, HttpServletResponse response) {
+        logger.info("Received POST request to /students");
         Optional<Student> responce = studentService.addStudent(requestDTO.getId(), requestDTO.getName(),
                 requestDTO.getAge());
         if (responce != null) {
@@ -49,6 +55,7 @@ public class HomeRestController {
 
     @PutMapping("/students")
     public ResponceStudentDTO updateStudent(@RequestBody RequestStudentDTO requestDTO) {
+        logger.info("Received PUT request to /students");
         Optional<Student> responce = studentService.updateStudent(requestDTO.getId(), requestDTO.getName(),
                 requestDTO.getAge());
         if (responce != null)
@@ -59,6 +66,7 @@ public class HomeRestController {
 
     @DeleteMapping("/students/")
     public Optional<Student> deleteStudentById(@RequestParam(required = true) Long id, HttpServletResponse response) {
+        logger.info("Received DELETE request to /students/?id=" + id);
         response.setStatus(HttpServletResponse.SC_NO_CONTENT);
         return studentService.deleteStudentById(id);
     }
