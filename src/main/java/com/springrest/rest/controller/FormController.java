f04550f970fd3aee99b487501136dcc6d64fa422
SergiyOl <ser4002@gmail.com>
2025-04-08 20:06:54 +0300

    Added /form page

---
+++ src/main/java/com/springrest/rest/controller/FormController.java
@@ -0,0 +1,18 @@
+package com.springrest.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FormController {
+
+    @Autowired
+    public FormController() {
+    }
+
+    @GetMapping("/form")
+    public String showForm() {
+        return "form";
+    }
+}
--- src/main/java/com/springrest/rest/security/SecurityConfig.java
+++ src/main/java/com/springrest/rest/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/home").permitAll()
                         .requestMatchers("/login").permitAll()
